345598b872b57dca93e304a4ccb1b7f99cb9963f
admin <admin@BVHO-DES-0033>
2022-02-12 10:49:04 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EmptyASPNetCoreMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmptyASPNetCoreMVC.Controllers
+{
+    public class HomeController: Controller
+    {
+        //public string Index()
+        //{
+        //    return "This is Index action from MVC Controller";
+        //}
+
+        //##Without Dipendancy Injection
+        //public JsonResult Index()
+        //{
+        //    TestStudentRepository repository = new TestStudentRepository();
+        //    List<Student> allStudentDetails = repository.GetAllStudent();
+        //    return Json(allStudentDetails);
+        //}
+        //public JsonResult GetStudentDetails(int Id)
+        //{
+        //    TestStudentRepository repository = new TestStudentRepository();
+        //    Student studentDetails = repository.GetStudentById(Id);
+        //    return Json(studentDetails);
+        //}
+
+        //##With Dependancy Injection
+        //private readonly IStudentRepository _repository = null;
+        //public HomeController(IStudentRepository repository)
+        //{
+        //    _repository = repository;
+        //}
+        //public JsonResult Index()
+        //{
+        //    List<Student> allStudentDetails = _repository.GetAllStudent();
+        //    return Json(allStudentDetails);
+        //}
+        //public JsonResult GetStudentDetails(int Id)
+        //{
+        //    Student studentDetails = _repository.GetStudentById(Id);
+        //    return Json(studentDetails);
+        //}
+        //Action Method Injectoin
+        public JsonResult Index([FromServices] IStudentRepository repository)
+        {
+            List<Student> allStudentDetails = repository.GetAllStudent();
+            return Json(allStudentDetails);
+        }
+    }
+}
---
+++ Models/IStudentRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmptyASPNetCoreMVC.Models
+{
+  public interface IStudentRepository
+    {
+        Student GetStudentById(int StudentId);
+        List<Student> GetAllStudent();
+    }
+}
